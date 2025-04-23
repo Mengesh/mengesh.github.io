845feda2d5f84926f563f529af4143cd8dbe4d80
Marko Radišić <marko@radisic.me>
2025-04-23 21:35:32 +0200

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://mengesh.github.io/astro-shadcn-ui-template',
-  base: '/astro-shadcn-ui-template',
-});
